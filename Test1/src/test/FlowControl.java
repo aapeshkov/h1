8d5c2de1de68717fbad883a3a863790540a6d56c
apeshkov <apeshkov@APESHKOV.cbg.local>
2018-03-26 18:01:28 +0300

    3

---
+++ Test1/src/test/FlowControl.java
@@ -0,0 +1,43 @@
+package test;
+
+public class FlowControl { // тело класса 
+public static void main(String[] args) {// тело метода
+	
+	int i = 10;
+	
+	{
+		System.out.println(i);
+		
+		int x = 20;
+		 // х существует только в рамках этого блока	
+		
+	}
+	// Здесь System.out.println(x); не сработает, здесь х не существует
+	
+	if (i <= 10) {
+		System.out.println("More");
+	}
+	
+	if (i > 10) {
+		System.out.println("Less");
+	} else {
+		System.out.println(i > 10 ? "Less" : "More");
+	} 
+	
+	if (i % 2 == 0) { //делится на 2 без остатка
+		System.out.println("Even");
+		i--;
+		}
+	System.out.println(i);
+	
+	if (i % 2 == 1){
+		System.out.println("Odd");
+		i++;
+	}
+	
+	if (i=10) {
+		System.out.println(i);
+	}
+	
+	}
+}
